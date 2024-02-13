46eb1e964b65ae1f72654cadeb9239896950a920
Imeanbusiness <dominic.adam.koh@gmail.com>
2024-02-13 10:43:14 -0800

    Added files to branch
    Files

---
+++ script.js
@@ -0,0 +1,6 @@
+function credits() {
+    alert("Made by 110% Dominic\nAlso made by -10% Teran")
+}
+function help() {
+    alert("Click buttons to view demo\nWords in all caps are important words\nClick on the title to go back")
+}
